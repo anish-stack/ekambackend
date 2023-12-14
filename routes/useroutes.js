0ddad17c3829ffafa5f5428bba624ccc12c34a2e
Anish jha <122715402+anish-stack@users.noreply.github.com>
2023-12-14 17:47:41 +0530

    Update useroutes.js

--- routes/useroutes.js
+++ routes/useroutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { signup, activateAccount, signin, logout, PasswordChange,getMyPurchase,contactForm } = require('../controllers/usercontroller')
 const { protect } = require('../middleware/auth')
 const { makePayment,getMakePyamentProductInfo } = require('../controllers/demoPaymentController')
+const { createPosition, getPostion, getSinglePostion, ApplyForJobPostion } = require('../controllers/PostionController')
 const router = express.Router()
 
 router.post('/SignUp',signup)
@@ -14,5 +15,9 @@ router.get("/getMakePyamentProductInfo/:productId",getMakePyamentProductInfo)
 router.get('/payment/do-for-paid/:productId',protect,makePayment)
 router.post('/Contact',contactForm)
 
+router.post('/Post-Position',createPosition)
+router.get('/get-Position',getPostion)
+router.get('/get-Position/:positionId',getSinglePostion)
+router.post('/Apply-job',ApplyForJobPostion)
 
 module.exports=router
