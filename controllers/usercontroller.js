a6fda24ab07bae798bf169f8c43ec4525a7454aa
Anish jha <122715402+anish-stack@users.noreply.github.com>
2023-12-28 21:14:16 +0530

    Update usercontroller.js

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -67,7 +67,7 @@ async function verificationLink(token, email, isActivate, resetTime) {
         // Save the user to the database
   
         // You can send the activation link in the response or via email to the user
-        const activationLink = `http://localhost:4000/api/v1/activate?token=${activationToken}&email=${encodeURIComponent(email)}`;
+        const activationLink = `https://ekambackend.onrender.com/api/v1/activate?token=${activationToken}&email=${encodeURIComponent(email)}`;
 
         const options = {
           email,
